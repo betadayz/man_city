3d141098c828c5cd989a6321beecf5d83f779615
unknown <betadayz>
2020-01-15 12:19:05 +0100

    auth public routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './Components/authRoutes/PrivateRoutes';
+import PublicRoute from './Components/authRoutes/PublicRoute';
 import Home from './Components/home';
 import SignIn from './Components/signIn';
 import Dashboard from './Components/admin/Dashboard';
@@ -11,10 +12,9 @@ const Routes = (props) => {
   return(
     <Layout>
       <Switch>
-         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
-         <Route exact component={Dashboard} path="/dashboard"/>
-         <Route exact component={SignIn} path="/sign_in"/> 
-         <Route exact component={Home} path="/"/>
+         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} /> 
+         <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn} />
+         <PublicRoute {...props} restricted={false} path="/" exact component={Home} />
       </Switch>
       
     </Layout>
